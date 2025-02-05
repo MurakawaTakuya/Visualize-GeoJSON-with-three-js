31217afff0d6cf961483cb6be41b385699ed7a7b
MurakawaTakuya <murakawatakuya5@gmail.com>
2025-02-05 21:24:04 +0900

    Analyticsを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import "./globals.scss";
@@ -18,6 +19,7 @@ export default function RootLayout({
         <main>
           {children}
           <SpeedInsights />
+          <Analytics />
         </main>
       </body>
     </html>
