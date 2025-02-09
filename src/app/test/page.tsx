94cd94603e79f869a4e7a40494d76f82902deb05
MurakawaTakuya <murakawatakuya5@gmail.com>
2025-02-09 20:53:13 +0900

    install tailwindcss

---
+++ src/app/test/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TestPage: React.FC = () => {
+  return (
+    <h1 className="text-3xl font-bold underline text-white">Hello world!</h1>
+  );
+};
+
+export default TestPage;
