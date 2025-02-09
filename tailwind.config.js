10b72efcdefa06d7253789d2332052617af462a1
MurakawaTakuya <murakawatakuya5@gmail.com>
2025-02-09 22:11:43 +0900

    install shadcn/ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,17 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {},
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [import("tailwindcss-animate")],
 };
